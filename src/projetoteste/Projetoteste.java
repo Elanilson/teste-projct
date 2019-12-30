968d1ce133731781fb0e634b97a0c475d597e10c
Elanilson <elanilsonpp@hotmail.com>
2019-12-30 01:04:43 -0200

    mensagem de boa noite

--- src/projetoteste/Projetoteste.java
+++ src/projetoteste/Projetoteste.java
@@ -16,6 +16,8 @@ public class Projetoteste {
      */
     public static void main(String[] args) {
         System.out.println("bom dia");
+        System.out.println(" boa tarde");
+        System.out.println("boa noite");
     }
     
 }
